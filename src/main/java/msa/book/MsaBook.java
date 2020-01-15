7b5dc33ebaac294db5834bb8e1f287844bc1ad80
DrEdwardPark <59864208+DrEdwardPark@users.noreply.github.com>
2020-01-15 11:17:20 +0900

    Add files via upload

---
+++ src/main/java/msa/book/MsaBook.java
@@ -0,0 +1,18 @@
+package msa.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MsaBook extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MsaBook.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MsaBook.class, args);
+	}
+}
